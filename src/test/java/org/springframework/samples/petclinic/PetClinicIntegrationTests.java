9a933ca5fd794cd49ea2d172980079a8fbf32ba8
mczyjs <156386743+mczyjs@users.noreply.github.com>
2024-04-09 15:27:14 +0800

    Update PetClinicIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -17,6 +17,7 @@
 package org.springframework.samples.petclinic;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
